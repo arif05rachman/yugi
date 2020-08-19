894f7a68a5f73ab44c118a66a4360b3ad8a765ea
Arif Rachman Husen <arif05rachman@gmail.com>
2020-08-19 15:01:15 +0700

    edited

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -16,17 +16,15 @@ export default function CardComponent(props) {
     race,
     type,
   } = props.card;
-  console.log(card_images[0].image_url_small);
+  // console.log(card_images[0].image_url_small);
   return (
     <Card style={{ width: "16rem" }} className="p-1 mb-2 mx-1">
-      <Card.Img variant="top" src={card_images[0].image_url} />
+      <Card.Img variant="top" src={card_images[0].image_url_small} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title style={{height: '2rem', fontSize:'1rem'}} className="font-weight-bold text-center">{name}</Card.Title>
+        <div>
+          <Button variant="primary">show</Button>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
 import NavbarComponent from '../Components/NavbarComponent'
-import CardComponent from '../Components/CardComponent'
+// import CardComponent from '../Components/CardComponent'
 import CarouselComponent from '../Components/CarouselComponent'
 import FooterComponent from '../Components/FooterComponent'
 import { Row, Container, Spinner } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCards } from '../store/actions'
+
+const CardComponent = React.lazy(() => import('../Components/CardComponent'))
 export default function Home() {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getCards('https://db.ygoprodeck.com/api/v7/cardinfo.php?&num=40&offset=40&sort=new'))
-  }, [])
+  }, [dispatch])
   const cards = useSelector(state => state.cards.cards)
 
   return (
@@ -20,7 +22,18 @@ export default function Home() {
       <hr className="border border-info w-75"></hr>
       <Container>
         <Row className="justify-content-center">
-          {cards.length > 0 ? (
+          <Suspense fallback={<div>
+            <h1 className="text-center text-light d-flex align-items-center">
+              <em> Loading </em> <Spinner animation="grow" variant="info" />
+            </h1>
+          </div>}>
+            {
+              cards.map((card) => {
+                return <CardComponent card={card} key={card.id} />;
+              })
+            }
+          </Suspense>
+          {/* {cards.length > 0 ? (
             cards.map((card) => {
               return <CardComponent card={card} key={card.id} />;
             })
@@ -30,7 +43,7 @@ export default function Home() {
                   <em> Loading </em> <Spinner animation="grow" variant="info" />
                 </h1>
               </div>
-            )}
+            )} */}
         </Row>
       </Container>
       <FooterComponent></FooterComponent>
